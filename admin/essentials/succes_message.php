4d7940b77700f55050801ba21516503c70312a45
Krukas <maikel_martens_1@hotmail.com>
2013-05-07 15:06:00 +0200

    Spell error
    spell error succes to success

--- admin/essentials/succes_message.php
+++
@@ -1,8 +0,0 @@
-<div class='succes'>
-	<div class='messageicon'>
-		<img class='icon' src='<?PHP echo base_url(__ADMIN_FOLDER.'/images/icons/succes.png') ?>' alt='' />
-	</div>
-	<div class='messagetext'>
-	 	<?PHP echo $_SESSION['OPC_admin_succes'] ?>
-	</div>
-</div>
---
+++ admin/essentials/success_message.php
@@ -0,0 +1,8 @@
+<div class='success'>
+	<div class='messageicon'>
+		<img class='icon' src='<?PHP echo base_url(__ADMIN_FOLDER.'/images/icons/success.png') ?>' alt='' />
+	</div>
+	<div class='messagetext'>
+	 	<?PHP echo $_SESSION['OPC_admin_succes'] ?>
+	</div>
+</div>
