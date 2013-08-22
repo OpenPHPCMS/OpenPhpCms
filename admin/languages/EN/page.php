9626d52cfe430632b45d82a2e7dc0cee460b7772
Krukas <maikel_martens_1@hotmail.com>
2013-08-22 14:49:05 +0200

    moved to application

--- admin/languages/EN/page.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('__SITE_PATH')) exit('No direct script access allowed');
-
-$language = array (
-	'page_name_empty' 			=> 'Cannot be empty!',
-	'page_name_invalid' 		=> 'Name is invalid can only contains alphanumeric and "-_" characters!',
-	'page_name_already_exists' 	=> 'Name already exists!',
-	'page_type_not_exists' 		=> 'Page type does not exists!',
-);
